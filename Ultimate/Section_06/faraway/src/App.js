7fbc83278e63184463ba51a0a0c53d658f8d04ca
LAXMAN <laxsingh9659@gmail.com>
2023-07-08 07:08:47 +0530

    Controlled Elements

--- Ultimate/Section_06/faraway/src/App.js
+++ Ultimate/Section_06/faraway/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -18,22 +19,43 @@ function Logo() {
   return <h1>🌴Far away💼</h1>;
 }
 function Form() {
+  const [desc, setDesc] = useState("Testing");
+  const [quantity, setQuantity] = useState(1);
   function handleSubmit(e) {
     e.preventDefault();
+
+    // guard clause 
+
+    if(!desc) return ;
+    const newItem={desc,quantity,packed:false,id:Date.now()};
+    // console.log(newItem);
+
+    
+
   }
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       {/* onSubmit will work for both 1.hitting Enter key 2.click Submit Button */}
       {/* onClick only work with the click Event */}
       <h3>What do you need for your 😍 trip</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => {
+          setQuantity(Number(e.target.value));
+        }}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={desc}
+        onChange={(e) => setDesc(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
---
+++ Ultimate/Section_06/faraway/src/Notes.md
@@ -0,0 +1,3 @@
+## Controlled Element in React 
+
+ 
