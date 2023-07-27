615aefdd74976032a2de4593844f87afb0c6a4ca
LAXMAN SINGH <115009552+LaxmanSingh07@users.noreply.github.com>
2023-07-27 19:21:19 +0530

    Instances and Element

--- Ultimate/Section_11/Notes.md
+++ Ultimate/Section_11/Notes.md
@@ -1,2 +1,36 @@
 ## How React Works `Behind the Scenes`
 
+### `Component vs Instance vs Element`
+
+
+ **Components** 
+
+        👉 Description of a piece of UI
+        👉 A component is a function that returns React elements (element tree), usually written as JSX
+        👉 “Blueprint” or “Template”
+
+**Instances**
+
+        👉 Instances are created when we “use” components
+        👉 React internally calls Tab()
+        👉 Actual “physical” manifestation of a component
+        👉 Has its own state and props
+        👉 Has a lifecycle (can “be born”, “live”, and “die”
+
+
+**Elements**
+
+    👉 JSX is converted to React.createElement() function calls
+    👉 A React element is the result of these function calls
+    👉 Information necessary to create DOM element
+
+
+```   
+     Component
+Component Instance
+React Element
+RETURNS
+DOM Element (HTML)
+INSERTED TO DOM
+
+```
---
+++ Ultimate/Section_11/demo/src/App.js
@@ -0,0 +1,108 @@
+import { useState } from "react";
+
+const content = [
+  {
+    summary: "React is a library for building UIs",
+    details:
+      "Dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    summary: "State management is like giving state a home",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    summary: "We can think of props as the component API",
+    details:
+      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+  },
+];
+
+
+// console.log(<DifferentContent test={30} />)
+// console.log(DifferentContent()); // never do this 
+
+export default function App() {
+  return (
+    <div>
+      <Tabbed content={content} />
+    </div>
+  );
+}
+
+function Tabbed({ content }) {
+  const [activeTab, setActiveTab] = useState(0);
+
+  return (
+    <div>
+      <div className="tabs">
+        <Tab num={0} activeTab={activeTab} onClick={setActiveTab} />
+        <Tab num={1} activeTab={activeTab} onClick={setActiveTab} />
+        <Tab num={2} activeTab={activeTab} onClick={setActiveTab} />
+        <Tab num={3} activeTab={activeTab} onClick={setActiveTab} />
+      </div>
+
+      {activeTab <= 2 ? (
+        <TabContent item={content.at(activeTab)} />
+      ) : (
+        <DifferentContent />
+      )}
+
+      {/* When we call the component directly react will no longer see it as a component instance  */}
+      {/* it is violting the rules of hooks */}
+      {TabContent({ item: content.at(0) })} 
+    </div>
+  );
+}
+
+function Tab({ num, activeTab, onClick }) {
+  return (
+    <button
+      className={activeTab === num ? "tab active" : "tab"}
+      onClick={() => onClick(num)}
+    >
+      Tab {num + 1}
+    </button>
+  );
+}
+
+function TabContent({ item }) {
+  const [showDetails, setShowDetails] = useState(true);
+  const [likes, setLikes] = useState(0);
+
+  function handleInc() {
+    setLikes(likes + 1);
+  }
+
+  return (
+    <div className="tab-content">
+      <h4>{item.summary}</h4>
+      {showDetails && <p>{item.details}</p>}
+
+      <div className="tab-actions">
+        <button onClick={() => setShowDetails((h) => !h)}>
+          {showDetails ? "Hide" : "Show"} details
+        </button>
+
+        <div className="hearts-counter">
+          <span>{likes} ❤️</span>
+          <button onClick={handleInc}>+</button>
+          <button>+++</button>
+        </div>
+      </div>
+
+      <div className="tab-undo">
+        <button>Undo</button>
+        <button>Undo in 2s</button>
+      </div>
+    </div>
+  );
+}
+
+function DifferentContent() {
+  return (
+    <div className="tab-content">
+      <h4>I'm a DIFFERENT tab, so I reset state 💣💥</h4>
+    </div>
+  );
+}
