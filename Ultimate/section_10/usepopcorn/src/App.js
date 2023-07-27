d78e630c0e3766afd5f2a11c74f550236059a723
LAXMAN SINGH <115009552+LaxmanSingh07@users.noreply.github.com>
2023-07-27 12:37:15 +0530

    Creating a reusable compoentn

--- Ultimate/section_10/usepopcorn/src/App.js
+++ Ultimate/section_10/usepopcorn/src/App.js
@@ -232,4 +232,3 @@ function WatchedMovieItem({ movie }) {
   )
 
 }
-
--- Ultimate/section_10/usepopcorn/src/index.js
+++ Ultimate/section_10/usepopcorn/src/index.js
@@ -10,7 +10,6 @@ root.render(
   <React.StrictMode>
     {/* <App /> */}
     <StarRating maxRating={5} />
-    <StarRating maxRating={10} />
   </React.StrictMode>
 );
 
