b3a912d23e06b67a3b25343ce1aac34dd4dc2e5b
LAXMAN SINGH <115009552+LaxmanSingh07@users.noreply.github.com>
2023-07-29 15:49:51 +0530

    adding a new effect

--- Ultimate/section_10/usepopcorn/src/App.js
+++ Ultimate/section_10/usepopcorn/src/App.js
@@ -350,6 +350,11 @@ function MovieDetails({
     [selectedId]
   );
 
+  useEffect(function () {
+    if(!title) return;
+    document.title = `Movie | ${title}`
+  },[title])
+
   return (
     <div className="details">
       {isLoading ? (
