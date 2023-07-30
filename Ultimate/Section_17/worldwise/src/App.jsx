a6d99b8429186f74e21ccdeec4973843c4dc67c0
LAXMAN SINGH <115009552+LaxmanSingh07@users.noreply.github.com>
2023-07-30 19:46:04 +0530

    Implementing maing pages and routes

--- Ultimate/Section_17/worldwise/src/App.jsx
+++ Ultimate/Section_17/worldwise/src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Product from "./pages/Product.jsx";
+import Pricing from "./pages/Pricing.jsx";
+import HomePage from "./pages/HomePage";
+import PageNotFound from './pages/PageNotFound.jsx';
 
 function App() {
   return (
-    <div>World</div>
-  )
+    <>
+      <h1>Hello World</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound/>} />
+      </Routes>
+      </BrowserRouter>
+      </>
+  );
 }
 
 export default App
---
+++ Ultimate/Section_17/worldwise/src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div>WorldWise</div>
+  )
+}
+
+export default HomePage
