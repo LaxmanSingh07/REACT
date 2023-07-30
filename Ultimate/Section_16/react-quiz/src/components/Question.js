7d8516ff7230383bd7d10efa8641bb8d192b1355
LAXMAN SINGH <115009552+LaxmanSingh07@users.noreply.github.com>
2023-07-30 12:44:41 +0530

    Display questions

---
+++ Ultimate/Section_16/react-quiz/src/components/Question.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Options from './Options';
+
+function Question({ question }) {
+  return (
+    <div>
+      <h4>{question.question}</h4>
+      <Options question={question} />
+    </div>
+  );
+}
+
+export default Question
