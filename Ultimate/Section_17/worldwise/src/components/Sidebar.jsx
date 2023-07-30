4b802bf57343f2de9b39a01bf7ef09446468d101
LAXMAN SINGH <115009552+LaxmanSingh07@users.noreply.github.com>
2023-07-30 22:44:57 +0530

    building App layout

---
+++ Ultimate/Section_17/worldwise/src/components/Sidebar.jsx
@@ -0,0 +1,21 @@
+import AppNav from "../pages/AppNav";
+import Logo from "./Logo";
+import styles from "./Sidebar.module.css";
+
+function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+
+      <p>List of cities</p>
+      <footer className={styles.footer}>
+        <p className={styles.copyright}>
+          &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default Sidebar;
--- Ultimate/Section_17/worldwise/src/pages/AppLayout.jsx
+++ Ultimate/Section_17/worldwise/src/pages/AppLayout.jsx
@@ -1,9 +1,16 @@
-import AppNav from "./AppNav"
+import Sidebar from "../components/Sidebar";
+import AppNav from "./AppNav";
+
+import styles from "./AppLayout.module.css";
+import Map from "../components/Map";
 
 function AppLayout() {
   return (
-   <AppNav/>
-  )
+    <div className={styles.app}>
+      <Sidebar />
+      <Map/>
+    </div>
+  );
 }
 
-export default AppLayout
+export default AppLayout;
