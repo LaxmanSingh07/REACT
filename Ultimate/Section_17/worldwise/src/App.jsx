9e12ac9b0d808673534ce3ddb85785c4ca7db17b
LAXMAN SINGH <115009552+LaxmanSingh07@users.noreply.github.com>
2023-07-31 21:26:26 +0530

    Programmatic Navigation with Navigate

--- Ultimate/Section_17/worldwise/src/App.jsx
+++ Ultimate/Section_17/worldwise/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import Product from "./pages/Product.jsx";
 import Pricing from "./pages/Pricing.jsx";
@@ -45,7 +45,7 @@ function App() {
           <Route path="app" element={<AppLayout />}>
             <Route
               index
-              element={<CityList cities={cities} isLoading={isLoading} />}
+              element={<Navigate to='cities' replace/>}
             />
             s
             <Route
