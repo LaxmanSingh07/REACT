f62874c027fa52e9639eea7852a0a2ab2d511b9a
LAXMAN SINGH <115009552+LaxmanSingh07@users.noreply.github.com>
2023-08-01 11:49:57 +0530

    Performance optimization and Wasted Renders

--- Ultimate/Section_17/worldwise/src/components/Map.jsx
+++ Ultimate/Section_17/worldwise/src/components/Map.jsx
@@ -1,25 +1,36 @@
-import React from 'react'
-import styles from './Map.module.css'
-import { useNavigate, useSearchParams } from "react-router-dom";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import React, { useEffect } from "react";
 import { useState } from "react";
+import styles from "./Map.module.css";
+import { useSearchParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { useCities } from "../contexts/CitiesContext";
-
+import { useMap } from "react-leaflet";
+import { useMapEvents } from "react-leaflet";
 function Map() {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   const { cities } = useCities();
 
   const [mapPosition, setMapPositon] = useState([40, 0]);
 
-  const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  const [searchParams] = useSearchParams();
+
+  const mapLat = searchParams.get("lat");
+  const mapLng = searchParams.get("lng");
+
+  useEffect(
+    function () {
+      if (mapLat && mapLng) setMapPositon([mapLat, mapLng]);
+    },
+    [mapLat, mapLng]
+  );
 
   return (
     <div className={styles.mapContainer}>
       <MapContainer
         center={mapPosition}
+        // center={[mapLat, mapLng]}
         zoom={13}
         scrollWheelZoom={true}
         className={styles.map}
@@ -31,7 +42,7 @@ function Map() {
         {cities.map((city) => (
           <Marker
             position={[city.position.lat, city.position.lng]}
-            key={city.id }
+            key={city.id}
           >
             <Popup>
               <span>{city.emoji}</span>
@@ -39,9 +50,27 @@ function Map() {
             </Popup>
           </Marker>
         ))}
+        <ChangeCenter position={mapPosition} />
+        <DetectClick />
       </MapContainer>
     </div>
   );
 }
 
-export default Map
+//this is our own component
+function ChangeCenter(position) {
+  const map = useMap();
+  map.setView(position);
+  return null;
+}
+
+//custom component
+
+function DetectClick() {
+  const navigate = useNavigate();
+  useMapEvents({
+    click: (e) => navigate(`form`),
+  });
+}
+
+export default Map;
---
+++ Ultimate/Section_18_19/Notes1.md
@@ -0,0 +1,28 @@
+## PERFORMANCE OPTIMIZATION TOOLS 
+
+### 1. Prevent wasted Renders 
+
+        👉 memo
+        👉 useMemo
+        👉 useCallback
+        👉 Passing element as children to regular prop
+
+### 2. Improve app speed/reponsiveness
+
+    👉 useMemo
+    👉 useCallback
+    👉 useTransition
+
+
+### 3. Reduce bundle size
+
+    👉 Using fewer 3rd package 
+    👉 code splitting and lazy loading 
+
+
+👋 This list of tools and techniques is, by no means, exhaustive. You’re already doing 
+many optimizations by following the best practices I have been showing you ✌
+
+## When Does a Component instance RE-RENDER? 
+
+![](./WasteRender.png)
