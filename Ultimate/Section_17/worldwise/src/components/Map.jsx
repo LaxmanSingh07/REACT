5ef75bfb7e9c4c821f02a6f29892c1967bb18e02
LAXMAN SINGH <115009552+LaxmanSingh07@users.noreply.github.com>
2023-08-01 11:01:11 +0530

    Displaying City Markers

--- Ultimate/Section_17/worldwise/src/components/Map.jsx
+++ Ultimate/Section_17/worldwise/src/components/Map.jsx
@@ -3,9 +3,12 @@ import styles from './Map.module.css'
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { useState } from "react";
+import { useCities } from "../contexts/CitiesContext";
 
 function Map() {
   const navigate = useNavigate();
+
+  const { cities } = useCities();
 
   const [mapPosition, setMapPositon] = useState([40, 0]);
 
@@ -25,11 +28,17 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id }
+          >
+            <Popup>
+              <span>{city.emoji}</span>
+              <span> {city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
