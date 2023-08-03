28cc26036a932520f36f29953291c92305d76f0b
LAXMAN SINGH <115009552+LaxmanSingh07@users.noreply.github.com>
2023-08-03 16:28:06 +0530

    Back to the WorldWise

--- Ultimate/Section_17_19/worldwise/src/components/City.jsx
+++ Ultimate/Section_17_19/worldwise/src/components/City.jsx
@@ -15,20 +15,19 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function City() {
-
   const { id } = useParams();
-  const { getCity, currentCity,isLoading } = useCities();
-  
-  
-  
-  useEffect(function () {
-    getCity(id);
+  const { getCity, currentCity, isLoading } = useCities();
 
-  }, [id]);
+  useEffect(
+    function () {
+      getCity(id);
+    },
+    [id,getCity]
+  );
 
   const { cityName, emoji, date, notes } = currentCity;
 
-  if (isLoading) return <Spinner />
+  if (isLoading) return <Spinner />;
   return (
     <div className={styles.city}>
       <div className={styles.row}>
@@ -62,7 +61,7 @@ function City() {
       </div>
 
       <div>
-      <BackButton/>
+        <BackButton />
       </div>
     </div>
   );
--- Ultimate/Section_17_19/worldwise/src/contexts/CitiesContext.jsx
+++ Ultimate/Section_17_19/worldwise/src/contexts/CitiesContext.jsx
@@ -1,4 +1,5 @@
-import { useContext, createContext, useState,useEffect  } from "react";
+import { useCallback } from "react";
+import { useContext, createContext, useState, useEffect } from "react";
 import { useReducer } from "react";
 const BASE_URL = "http://localhost:9000";
 
@@ -83,20 +84,23 @@ function CitiesProvider({ children }) {
     fetchCities();
   }, []);
 
-  async function getCity(id) {
-    if (Number(id) === currentCity.id) return;
-    dispatch({ type: "loading" });
-    try {
-      const res = await fetch(`${BASE_URL}/cities/${id}`);
-      const data = await res.json();
-      dispatch({ type: "city/loaded", payload: data });
-    } catch {
-      dispatch({
-        type: "rejected",
-        payload: "There was an error loading the city ..",
-      });
-    }
-  }
+  const getCity = useCallback(
+    async function getCity(id) {
+      if (Number(id) === currentCity.id) return;
+      dispatch({ type: "loading" });
+      try {
+        const res = await fetch(`${BASE_URL}/cities/${id}`);
+        const data = await res.json();
+        dispatch({ type: "city/loaded", payload: data });
+      } catch {
+        dispatch({
+          type: "rejected",
+          payload: "There was an error loading the city ..",
+        });
+      }
+    },
+    [currentCity.id]
+  );
   async function createCity(newCity) {
     dispatch({ type: "loading" });
     try {
