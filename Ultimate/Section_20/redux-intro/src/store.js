8867dc806d4a01dda42783642b0cf57aca182e5f
LAXMAN SINGH <115009552+LaxmanSingh07@users.noreply.github.com>
2023-08-03 19:37:48 +0530

    CREATING A REDUX STORE

--- Ultimate/Section_20/redux-intro/src/store.js
+++ Ultimate/Section_20/redux-intro/src/store.js
@@ -1,3 +1,5 @@
+import { createStore } from "redux";
+
 const initialState = {
   balance: 0,
   loan: 0,
@@ -12,10 +14,44 @@ function reducer(state = initialState, action) {
       return { ...state, balance: state.balance - action.payload };
     case "account/requestLoan":
       if (state.loan > 0) return state;
-      return { ...state, loan: action.payload.purpose };
-      case "account/payLoan":
-        return { ...state, loan: 0,loanPurpose:"",balance:state.balance-state.loan };
-      default:
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+    default:
       return state;
   }
 }
+
+// I WILL CHANGE IT SOON
+const store = createStore(reducer);
+
+store.dispatch({ type: "account/deposit", payload: 1000 });
+
+// console.log("Hello Redux");
+
+console.log(store.getState());
+
+store.dispatch({ type: "account/widthdraw", payload: 500 });
+
+console.log(store.getState());
+
+store.dispatch({
+  type: "account/requestLoan",
+  payload: { amount: 1000, purpose: "buy a car" },
+});
+
+console.log(store.getState());
+
+store.dispatch({ type: "account/payLoan" });
+
+console.log(store.getState());
