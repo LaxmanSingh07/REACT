4364ea51dd752e0d33534a0b4adbd6069f3f2423
LAXMAN SINGH <115009552+LaxmanSingh07@users.noreply.github.com>
2023-08-04 22:54:42 +0530

    Second last

--- Ultimate/Section_21/FastReactPizza/src/features/order/CreateOrder.jsx
+++ Ultimate/Section_21/FastReactPizza/src/features/order/CreateOrder.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import {Form} from "react-router-dom"
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -38,7 +39,8 @@ function CreateOrder() {
     <div>
       <h2>Ready to order? Let's go!</h2>
 
-      <form>
+      {/* HERE GET WILL NOT WORK IN THE WAY WE ARE IMPLEMENTING  */}
+      <Form method="POST" >
         <div>
           <label>First Name</label>
           <input type="text" name="customer" required />
@@ -72,7 +74,7 @@ function CreateOrder() {
         <div>
           <button>Order now</button>
         </div>
-      </form>
+      </Form>
     </div>
   );
 }
