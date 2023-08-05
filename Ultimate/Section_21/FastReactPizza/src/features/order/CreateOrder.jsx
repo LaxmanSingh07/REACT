100b1bdb8dbaa03533f72fadc3fdf7f1d17b0259
LAXMAN SINGH <115009552+LaxmanSingh07@users.noreply.github.com>
2023-08-05 09:42:45 +0530

    Styling Form Elements

--- Ultimate/Section_21/FastReactPizza/src/features/order/CreateOrder.jsx
+++ Ultimate/Section_21/FastReactPizza/src/features/order/CreateOrder.jsx
@@ -56,12 +56,19 @@ function CreateOrder() {
         <div>
           <label>Address</label>
           <div>
-            <input type="text" name="address" required />
+            <input
+              type="text"
+              name="address"
+              className="w-full rounded-full border border-stone-200 px-4 py-2 text-sm placeholder-stone-400
+              transition-all duration-300 focus:outline-none focus:ring focus:ring-yellow-400 md:px-6 md:py-3"
+              required
+            />
           </div>
         </div>
 
         <div>
           <input
+            className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
             type="checkbox"
             name="priority"
             id="priority"
--- Ultimate/Section_21/FastReactPizza/src/features/order/SearchOrder.jsx
+++ Ultimate/Section_21/FastReactPizza/src/features/order/SearchOrder.jsx
@@ -17,15 +17,16 @@ function SearchOrder() {
 
     }
     return (
-        <form onSubmit={handleSubmit}>
-            
-    <input
-      placeholder="Search Order #"
-      value={query}
-      onChange={(e) => setQuery(e.target.value)}
-            />
-            </form>
-  );
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Search Order #"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          className="w-28 rounded-full bg-yellow-100 px-4 py-3
+           text-sm transition-all duration-100 placeholder:text-stone-400 focus:w-72 focus:outline-none  focus:ring focus:ring-yellow-500 focus:ring-opacity-50 sm:w-64"
+        />
+      </form>
+    );
 }
 
 export default SearchOrder;
