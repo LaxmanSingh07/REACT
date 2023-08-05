2d03d2bc247c60c3ec0958527d58289fcb3eb8f2
LAXMAN SINGH <115009552+LaxmanSingh07@users.noreply.github.com>
2023-08-05 16:04:30 +0530

    Global styles with Styled Component

--- Ultimate/Section_25/the-wild-oasis/src/App.jsx
+++ Ultimate/Section_25/the-wild-oasis/src/App.jsx
@@ -1,27 +1,12 @@
 import { styled } from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
 
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: bold;
   background-color: yellowgreen;
-`;
-
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem, 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
-  margin: 20px;
-  cursor: pointer;
-`;
-
-const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.8rem 1.2rem;
 `;
 
 const StyledApp = styled.div`
@@ -31,11 +16,15 @@ const StyledApp = styled.div`
 
 function App() {
   return (
-    <StyledApp>
-      <H1>Styled Components</H1>
-      <Button onClick={() => alert("check in")}>Click Me</Button>
-      <Input type="text" placeholder="Enter your name" />
-    </StyledApp>
+    <>
+      {/* it doesn't accept any children */}
+      <GlobalStyles />;
+      <StyledApp>
+        <H1>Styled Components</H1>
+        <Button onClick={() => alert("check in")}>Click Me</Button>
+        <Input type="text" placeholder="Enter your name" />
+      </StyledApp>
+    </>
   );
 }
 
