4c7f4359d80e57f4670de7682b714c9992633a81
LAXMAN SINGH <115009552+LaxmanSingh07@users.noreply.github.com>
2023-08-05 09:28:43 +0530

    Styling Button element state and transition

--- Ultimate/Section_21/FastReactPizza/src/features/order/CreateOrder.jsx
+++ Ultimate/Section_21/FastReactPizza/src/features/order/CreateOrder.jsx
@@ -1,30 +1,30 @@
-import { useState } from "react";
-import {Form} from "react-router-dom"
+import { useState } from 'react';
+import { Form } from 'react-router-dom';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-    str
+    str,
   );
 
 const fakeCart = [
   {
     pizzaId: 12,
-    name: "Mediterranean",
+    name: 'Mediterranean',
     quantity: 2,
     unitPrice: 16,
     totalPrice: 32,
   },
   {
     pizzaId: 6,
-    name: "Vegetale",
+    name: 'Vegetale',
     quantity: 1,
     unitPrice: 13,
     totalPrice: 13,
   },
   {
     pizzaId: 11,
-    name: "Spinach and Mushroom",
+    name: 'Spinach and Mushroom',
     quantity: 1,
     unitPrice: 15,
     totalPrice: 15,
@@ -40,7 +40,7 @@ function CreateOrder() {
       <h2>Ready to order? Let's go!</h2>
 
       {/* HERE GET WILL NOT WORK IN THE WAY WE ARE IMPLEMENTING  */}
-      <Form method="POST" >
+      <Form method="POST">
         <div>
           <label>First Name</label>
           <input type="text" name="customer" required />
@@ -72,7 +72,9 @@ function CreateOrder() {
         </div>
 
         <div>
-          <button>Order now</button>
+          <button className="transition- focus:rin inline-block rounded-full bg-yellow-400 px-4 py-3  font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed">
+            Order now
+          </button>
         </div>
       </Form>
     </div>
