8a704454327164561997310086f29224ff6f4c2d
LAXMAN SINGH <115009552+LaxmanSingh07@users.noreply.github.com>
2023-08-05 08:10:32 +0530

    Responsive Design

--- Ultimate/Section_21/FastReactPizza/src/ui/Home.jsx
+++ Ultimate/Section_21/FastReactPizza/src/ui/Home.jsx
@@ -2,7 +2,8 @@ import CreateUser from '../features/user/CreateUser';
 
 function Home() {
   return (
-    <div className="my-10 text-center ">
+    // i am learning the mobile first approach
+    <div className="my-10 text-center sm:my-16">
       <h1 className="mb-8 text-xl font-semibold">
         The best pizza.
         <br />
