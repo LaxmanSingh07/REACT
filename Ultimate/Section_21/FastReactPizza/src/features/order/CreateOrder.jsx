e304c9f19c3b34d3a88b4cea11ca059207d711b3
LAXMAN SINGH <115009552+LaxmanSingh07@users.noreply.github.com>
2023-08-05 12:59:07 +0530

    Styling the Order Form

--- Ultimate/Section_21/FastReactPizza/src/features/order/CreateOrder.jsx
+++ Ultimate/Section_21/FastReactPizza/src/features/order/CreateOrder.jsx
@@ -37,31 +37,37 @@ function CreateOrder() {
   const cart = fakeCart;
 
   return (
-    <div>
-      <h2>Ready to order? Let's go!</h2>
+    <div className="px-4 py-6">
+      <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
 
       {/* HERE GET WILL NOT WORK IN THE WAY WE ARE IMPLEMENTING  */}
       <Form method="POST">
-        <div>
-          <label>First Name</label>
-          <input type="text" name="customer" className="input" required />
+        <div className=" mb-5 flex flex-col gap-2  sm:flex-row sm:items-center">
+          <label className="sm:basis-40">First Name</label>
+          <input type="text" name="customer" className="input grow" required />
         </div>
 
-        <div>
-          <label>Phone number</label>
-          <div>
-            <input type="tel" name="phone" className="input" required />
+        <div className=" mb-5 flex flex-col gap-2  sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Phone number</label>
+          <div className="grow">
+            <input type="tel" name="phone" className="input w-full" required />
+            {/* {formErrors?.phone && <p className="text-sx mt-2 text-red-700 bg-red-100 p-2 rounded-md">{formErrors.phone}} */}
           </div>
         </div>
 
-        <div>
-          <label>Address</label>
-          <div>
-            <input type="text" name="address" className="input" required />
+        <div className=" mb-5 flex flex-col gap-2  sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Address</label>
+          <div className="grow">
+            <input
+              type="text"
+              name="address"
+              className="input w-full"
+              required
+            />
           </div>
         </div>
 
-        <div>
+        <div className="mb-12 flex items-center gap-5">
           <input
             className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
             type="checkbox"
@@ -70,7 +76,9 @@ function CreateOrder() {
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="priority">Want to yo give your order priority?</label>
+          <label htmlFor="priority" className="font-medium">
+            Want to yo give your order priority?
+          </label>
         </div>
 
         <div>
