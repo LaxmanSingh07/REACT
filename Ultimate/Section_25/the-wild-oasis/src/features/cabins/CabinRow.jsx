825df9f4e238bc8250f92176a05090aa18ec6a08
LAXMAN SINGH <115009552+LaxmanSingh07@users.noreply.github.com>
2023-08-06 11:41:25 +0530

    Mutations Deleting a Cabin

--- Ultimate/Section_25/the-wild-oasis/src/features/cabins/CabinRow.jsx
+++ Ultimate/Section_25/the-wild-oasis/src/features/cabins/CabinRow.jsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteCabin } from "../../services/apiCabins";
 
 const TableRow = styled.div`
   display: grid;
@@ -41,15 +43,38 @@ const Discount = styled.div`
 `;
 
 function CabinRow({ cabin }) {
-  const { name, maxCapacity, regularPrice, discount, image } = cabin;
+  const {
+    id: CabinId,
+    name,
+    maxCapacity,
+    regularPrice,
+    discount,
+    image,
+  } = cabin;
+
+  const queryClient = useQueryClient();
+
+  const { isLoading: isDeleting, mutate } = useMutation({
+    mutationFn: deleteCabin,
+    onSuccess: () => {
+      alert("Cabin deleted");
+      // invalidate the cache
+      queryClient.invalidateQueries({
+        queryKey: ["cabins"],
+      });
+    },
+    onError: (err) => alert(err.message),
+  });
   return (
     <TableRow role="row">
       <Img src={image} />
       <Cabin>{name}</Cabin>
       <div>Fits up to {maxCapacity}</div>
       <Price>{formatCurrency(regularPrice)}</Price>
-      <Discount>{formatCurrency(regularPrice)}</Discount>
-      <button>Delete</button>
+      <Discount>{formatCurrency(discount)}</Discount>
+      <button onClick={() => mutate(CabinId)} disabled={isDeleting}>
+        Delete
+      </button>
     </TableRow>
   );
 }
