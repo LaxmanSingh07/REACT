0ab67807269f1d5e02722d64846f8aec767bac7d
LAXMAN SINGH <115009552+LaxmanSingh07@users.noreply.github.com>
2023-08-06 10:38:04 +0530

    Setting up React Query

--- Ultimate/Section_25/the-wild-oasis/src/App.jsx
+++ Ultimate/Section_25/the-wild-oasis/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
+import Dashboard from "./pages/Dashboard";
 import GlobalStyles from "./styles/GlobalStyles";
 import Bookings from "./pages/Bookings";
 import Cabins from "./pages/Cabins";
@@ -11,9 +13,19 @@ import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // staleTime is the time that data in the cache is considered fresh
+      staleTime: 60 * 1000, // 60 seconds
+    },
+  },
+});
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -31,7 +43,7 @@ function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
 
