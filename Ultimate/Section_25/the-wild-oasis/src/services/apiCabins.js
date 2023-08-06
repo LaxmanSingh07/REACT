8d8c7d5fafea40db513ac8c2626ad35249dcb921
LAXMAN SINGH <115009552+LaxmanSingh07@users.noreply.github.com>
2023-08-06 08:38:07 +0530

    Connecting supabse with Our React App

---
+++ Ultimate/Section_25/the-wild-oasis/src/services/apiCabins.js
@@ -0,0 +1,11 @@
+import supabase from "./supaBase";
+
+export async function getCabins() {
+  const { data, error } = await supabase.from("cabins").select("*");
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabins could not be fetched");
+  }
+  return data;
+}
---
+++ Ultimate/Section_25/the-wild-oasis/src/services/supaBase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://coflxnluofzrhctscwpu.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNvZmx4bmx1b2Z6cmhjdHNjd3B1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTEyNTYwMzUsImV4cCI6MjAwNjgzMjAzNX0.jr1zZZQ4IPrtErmz2pUllQFH0ESxcKL79VXsi0lT-xk";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
